13eb057976cc7fdc1ea7a13e66c7ddf7fa646a79
renjiniag <60401154+renjiniag@users.noreply.github.com>
2020-02-08 11:20:13 -0800

    Update xmas-tree.py

--- xmas-tree.py
+++ xmas-tree.py
@@ -1,21 +1,19 @@
+row=int(input("Enter number of rows for the tree : "))
+
 for j in range(row,0,-1):
     for k in range(row-j,0,-1):
         print(" ",end="")
     for i in range(j,0,-1):
-        print("*",end=" ")    
-    print()
-    for k in range(row-j+1,0,-1):
-        print(" ",end="")
-    for i in range(j,1,-1):
-        print("-",end=" ") 
-    print()
-
-    ''' Output
+        if i==1:
+            endline="\n"
+        else:
+            endline="_"
+        print("*",end=endline)    
+           
+''' Output
 Enter number of rows for the tree : 4
-* * * *
- - - -
- * * *
-  - -
-  * *
-   -
+*_*_*_*
+ *_*_*
+  *_*
+   *
 '''   
